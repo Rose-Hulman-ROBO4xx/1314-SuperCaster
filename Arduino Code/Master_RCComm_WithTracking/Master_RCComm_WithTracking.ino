c74ad0d766bcc0a7cc858425e659c4c27b7f9f7a
ryanlf <ryanlf@rose-hulman.edu>
2014-05-18 21:57:36 -0400

    Updated comments on code
    Comments on Master RC Comm

--- Arduino Code/Master_RCComm_WithTracking/Master_RCComm_WithTracking.ino
+++ Arduino Code/Master_RCComm_WithTracking/Master_RCComm_WithTracking.ino
@@ -24,12 +24,12 @@ Sensor characteristics:
         150	4.92		0.46        ~4
 */
 
-//Imports
+/////////////////////////////Imports
 #include <stdlib.h>
 #include <Servo.h> b
 #include <EEPROM.h>
 
-//Constants 
+/////////////////////////Constants 
   //Sensors:
 #define NUM_US 8
 #define NUM_IR 5
@@ -37,6 +37,7 @@ Sensor characteristics:
 
 #define US_HIT_BUFFER 5
 #define IR_HIT_BUFFER 1
+#define SENSOR_READING_DELAY 100 //delay between sensor readings in ms
 
 #define CLOSE_INCHES 30 //Approximate stopping distance for US
 #define FAR_INCHES 50    //Approx starting distance for US
@@ -112,39 +113,34 @@ Sensor characteristics:
 #define LED_BP_FLAG 34
 
 
-//Antenna Defines
-//////ANT STUFF
+
+//////Tracking defines
+//Pins
 #define ANT_WAVEFORM 2
-
- 
-    // variable to store the servo position 
-#define MIN_POS 750 //120 Degrees
-#define MAX_POS 1850  //240 Degrees
-#define DELAY 2
-#define DEFAULT_SERVO_M 1275
-#define N180_DEG_M 1275
-#define DEFAULT_SERVO 90 
-#define STEP 5
-#define SERVO_TURN 60
-#define TURN_ANG_MAX 210
-#define TURN_ANG_MIN 150
-#define MIN_ANG 120
-#define MAX_ANG 240
-
-
 #define SPEAKER_FROM_WALKIETALKIE A15 //set input pin
 #define CALIBRATE_IN 44
 #define RADIO_POWER 24
 #define RADIO_VOL_UP 26
 #define RADIO_VOL_DWN 28
 #define SERVO_PIN 3
+ 
+// Servo defines
+#define MIN_POS 750 //120 Degrees. Servo PWM in microseconds required to reach minimum usable angle (120 at current settings)
+#define MAX_POS 1850  //240 Degrees. Servo PWM in microseconds required to reach maximum usable angle (240 at current settings)
+#define DELAY 2 //delay between PWM writes to servo. Slows down turn
+#define DEFAULT_SERVO_M 1275 //Default servo angle in microseconds PWM.
+#define DEFAULT_SERVO 90 //default servo angle in degrees
+#define STEP 5  //amount servo adds when trying to reach goal angle. in microseconds
+#define SERVO_TURN 60 //amount to add to PWM values to sevo when tracking. in microseconds
+#define MIN_ANG 120 //servo min in degrees
+#define MAX_ANG 240 //servo max in degrees
 
+//tracking parameters
 #define buffering 10 //what counts as straight ahead? If too small, the robot will jitter. If too large the robot will drive away from the transmitter
 #define sample_delay 10 //Number of edges detected before polling for value
 #define antenna_sample_size 3  //Number of samples taken before decision is made.
 #define STOP_LEVEL_MAX 120
 #define STOP_LEVEL_MIN 0
-
 
 //Enumerators for direction determination
 #define STOP_MOV 1
@@ -154,7 +150,7 @@ Sensor characteristics:
 #define ERROR_CALC -1
 
 
-//Global Variables
+////////////////////////////Global Variables
   //Sensors:
 boolean US_flag = 0;
 boolean IR_flag = 0;
@@ -202,7 +198,7 @@ int Anglei = 180;  //Desired Angle
 int trackSpeed = 0;
 
 
-//Tracking Vars
+//Tracking Variables
 
 uint16_t caliset = 0;
 volatile uint16_t voltage_1 = 0;
@@ -212,11 +208,10 @@ volatile int edge_count = 0;
 int goal_ang_micro = DEFAULT_SERVO_M;
 volatile boolean finished_move = 1;
 volatile int interrupt_count_servo = 0;
-volatile int current_pos_micro = N180_DEG_M;
+volatile int current_pos_micro = DEFAULT_SERVO_M;
 boolean move_enabled = 1; //0 locks antenna to default pos
 boolean toggle = 0;
 boolean second_sample_flag = 0;
-volatile int estate = LOW;
 int angle_2_casters = 180;
 Servo antenna_servo;
 boolean ant_read_flag = false;
@@ -227,7 +222,8 @@ uint16_t voltageReadings_2[antenna_sample_size]; //array for falling edge sample
 boolean drive_command = STOP_MOV;
 
 void setup() {
-  pinMode(KILL_PIN,OUTPUT);
+  
+  pinMode(KILL_PIN,OUTPUT); //start kill pin
   digitalWrite(KILL_PIN,LOW);
   
   pinMode(RC_PIN,INPUT_PULLUP);
@@ -238,7 +234,6 @@ void setup() {
     pinMode(US_pins[i],INPUT);    
     pinMode(US_enables[i],OUTPUT);
     
-    //digitalWrite(US_LEDs[i],LOW);
     
     //Enable group 1 initially
     if(i<3){
@@ -248,15 +243,15 @@ void setup() {
     }  
   }
   
-  for(i=0;i<NUM_IR;i++){
+  for(i=0;i<NUM_IR;i++){ //setup IR
     pinMode(IR_pins[i],INPUT);
   }
   
-  for(i=0; i< NUM_B; i++){
+  for(i=0; i< NUM_B; i++){ //setup bump sensors
     pinMode(B_pins[i],INPUT);
   }
   
-  pinMode(LED_US_FLAG,OUTPUT);  
+  pinMode(LED_US_FLAG,OUTPUT);   //debug LEDs
   digitalWrite(LED_US_FLAG,LOW);
   
   pinMode(LED_IR_FLAG,OUTPUT);  
@@ -282,29 +277,14 @@ void setup() {
   
   powerUpRadio();
 
-  //Serial.println("Set Output mode");
   antenna_servo.attach(3);  // attaches the servo on pin 9 to the servo object
   antenna_servo.writeMicroseconds(current_pos_micro);
-  //Serial.println("Set up Servo,attempting timer");
   
   byte HByte =  EEPROM.read(1);
   byte LByte =  EEPROM.read(2);
   caliset = word(HByte, LByte);
-//  Serial.print("EEPROM Calibration number: ");
-//  Serial.print(caliset);
-//  Serial.println(" If you haven't calibrated yet, you need to for it to work");
   
-  cli();
-  //set timer1 interrupt TIMER 1 INTERRUPTS DEDICATED TO SERVO
-//  TCCR1A = 0;// set entire TCCR1A register to 0
-//  TCCR1B = 0;// same for TCCR1B
-//  TCNT1  = 0;//initialize counter value to 0
-//  OCR1A = 1563;// = (16*10^6) / (1*1024) - 1 (must be <65536)  
-//  TCCR1B |= (1 << WGM12); // turn on CTC mode  
-//  TCCR1B |= (1 << CS12) | (1 << CS10);  // Set CS12 and CS10 bits for 1024 prescaler  
-//  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
-//  sei();//allow interrupts
-  
+  cli(); //disable interupts
   
    //set timer2 interrupt at 1kHz
   TCCR2A = 0;// set entire TCCR2A register to 0
@@ -334,7 +314,7 @@ void loop(){
   Serial.print('1'); // Tell Remote Control that master arduino is ready to communicate 
   delay(1);
   
-  if(stringComplete){ 
+  if(stringComplete){// if arduino has recived full command, process new command and set appropriate variables
       Ei = StringToInt(E);
       Ti = StringToInt(T);
       Si = StringToInt(S);
@@ -346,12 +326,12 @@ void loop(){
       stringComplete = false;
  
       
-    if(Ei==0){
+    if(Ei==0){ // emergency stop flag, kill power
        killPower();
        
     }
     
-    if(Si==1){    
+    if(Si==1){    // tracking enabled, enter tracking
       //Run Tracking and enable all safety sensors
       if(timer_flag){  
         updateTrackingSensors();
@@ -359,8 +339,8 @@ void loop(){
       
       //Antenna Readings, no caster movement, calculate tracking angle
       angle_2_casters = map(current_pos_micro,MIN_POS,MAX_POS,MAX_ANG,MIN_ANG);
-        if(samples_full){
-          if(Ti) move_enabled = 1;
+        if(samples_full){//if antenna buffer is full process data
+          if(Ti) move_enabled = 1; //determine tracking mode
           else move_enabled = 0;
           handleAntennaReadings();
           if(!Ti) {current_pos_micro = DEFAULT_SERVO_M;
@@ -368,34 +348,31 @@ void loop(){
         }
         
       if((!B_flag)&&(!US_flag)&&(!IR_flag)&&(Ei==1)){
-        trackSpeed = Verti;      
+        trackSpeed = Verti; //process tracking speed      
        
        if (Ti) {
        //Strafe
        
-        RightPICSendSerial(angle_2_casters, trackSpeed);
+        RightPICSendSerial(angle_2_casters, trackSpeed);//set angle to servo angle
         LeftPICSendSerial(angle_2_casters, trackSpeed);
        }  else {
          
          if(drive_command == DEC_ANG) {//right turn
-      // Serial.println("R");
-        RightPICSendSerial(180, STOP_SPD);
-        LeftPICSendSerial(180, trackSpeed);
+          RightPICSendSerial(180, STOP_SPD);
+          LeftPICSendSerial(180, trackSpeed);
        } else if (drive_command == INC_ANG) {//turn left
-     //  Serial.println("L");
-        RightPICSendSerial(180, trackSpeed);
-        LeftPICSendSerial(180, STOP_SPD);
-       } else if(drive_command == HOLD_ANG) {
-       //  Serial.println("H");
-        RightPICSendSerial(180, trackSpeed);
-        LeftPICSendSerial(180, trackSpeed);        
+          RightPICSendSerial(180, trackSpeed);
+          LeftPICSendSerial(180, STOP_SPD);
+       } else if(drive_command == HOLD_ANG) {//hold straight
+          RightPICSendSerial(180, trackSpeed);
+          LeftPICSendSerial(180, trackSpeed);        
        } else {
-         RightPICSendSerial(angle_2_casters, STOP_SPD);
+         RightPICSendSerial(angle_2_casters, STOP_SPD); //stop casters if no signal detected
         LeftPICSendSerial(angle_2_casters, STOP_SPD);
        }
        }      
       }else{
-        RightPICSendSerial(angle_2_casters, STOP_SPD);
+        RightPICSendSerial(angle_2_casters, STOP_SPD); //stop for sensor reading
         LeftPICSendSerial(angle_2_casters, STOP_SPD);
       }
       
@@ -409,7 +386,7 @@ void loop(){
       goal_ang_micro = DEFAULT_SERVO_M;
       if((!B_flag)&&(!IR_flag)&&(Ei == 1)){
         //If vert value not to extreme, and horz value is, perform an appropriate tank drive turn
-        if(Verti < FWD_LIMIT){
+        if(Verti < FWD_LIMIT){ //process controller input to set caster to correct values
           if(Verti > BWD_LIMIT){
             if(Horzi > LEFT_TURN_LIMIT){ 
                 if(Ti){                
@@ -450,42 +427,26 @@ void loop(){
   }
 }
   
-void killPower(){
+void killPower(){ //kills power to robot.
   digitalWrite(KILL_PIN,HIGH);
   RightPICSendSerial((Anglei*0.352), STOP_SPD);
   LeftPICSendSerial((Anglei*0.352), STOP_SPD);
-  /*
-  while(1){    
-    delay(1000);
-    Serial.print(1);
-    Serial.print("\nIR flag: ");
-    Serial.print(IR_flag);
-    if(IR_flag){
-      Serial.print("  IR location: ");
-      Serial.print(IR_location);
-    }
-    Serial.print("\n  E stop: ");
-    Serial.print(Ei);  
-    
-    IR_location = readIR();
-    if((!IR_flag)&&(Ei)){
-      return;
-    }
-  }*/
+  
 }
 
-void updateTrackingSensors(){ 
+void updateTrackingSensors(){ //called in loop, updates tracking sensors checked every .1 seconds
   IR_location = readIR();
   US_location = readUS();
   B_location = readB();
   timer_flag = 0;      
   
-  //Stop_flag = IR_flag|US_flag;
+  //any flags trigger appropriate responses
+  
   if(IR_flag){
     killPower();
   }
   if(B_flag){
-    //digitalWrite(US_LEDs[US_location], HIGH);
+    
     digitalWrite(LED_BP_FLAG,B_flag);
     RightPICSendSerial(angle_2_casters, STOP_SPD);
     LeftPICSendSerial(angle_2_casters, STOP_SPD);
@@ -495,25 +456,15 @@ void updateTrackingSensors(){
     RightPICSendSerial(angle_2_casters, STOP_SPD);
     LeftPICSendSerial(angle_2_casters, STOP_SPD);
   }else{
-    /*for(int i=0; i<NUM_US; i++){
-      digitalWrite(US_LEDs[i], LOW);
-    }*/
     digitalWrite(LED_US_FLAG,LOW);
     digitalWrite(LED_BP_FLAG,LOW);
   }
 }
 
-void updateRCSensors(){ 
+void updateRCSensors(){ //Updates remote control sensors, flags appropriate response checked every .1 seconds 
   IR_location = readIR();
   B_location = readB();
   timer_flag = 0;      
-  
-  
-  /*delay(20);
-  Serial.print("\n IRL:");
-  Serial.print(IR_flag);
-  Serial.print("\n");*/
-  
   
   //Stop_flag = IR_flag|US_flag;
   if(IR_flag){
@@ -544,17 +495,7 @@ int readB(){
 
 //Reads all values of the ultrasonic sensors, returns the array index of a sensor detecting an object
 int readUS(){
-  int tempVal=0;    
-  /*
-  for(i=0;i<NUM_US;i++){
-    US_read[i] = analogRead(US_pins[i]);
-    if(US_read[i] < US_CLOSE){
-      US_hitCount[i]++;
-    }else if(US_read[i] > US_FAR){
-      US_hitCount[i] = 0;
-    }
-  }*/
-  
+  int tempVal=0;      
   //Read thrid US group
     
   if(US_grpCount == 2){
@@ -619,34 +560,7 @@ int readUS(){
      if(US_hitCount[i]>US_HIT_BUFFER){
         US_hitCount[i] = US_HIT_BUFFER+1;  
         US_flag = 1;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//        Serial.print('\n');
-//        Serial.print(i);
-//        Serial.print('\n');
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+       
         return i;
      }
   }  
@@ -662,29 +576,10 @@ int readIR(){
     if(tempVal < IR_EDGE){
       IR_flag = 1;
       
-      //Serial.print('\n');      
-      //Serial.print(i);
-      //Serial.print('  ');
-      //Serial.print(tempVal);
-      //Serial.print("     ");
-      
       return i;
     }
   }
-  /*
-  for(i=0;i<NUM_IR;i++){ 
-     if(IR_read[i] < IR_EDGE){
-        IR_hitCount[i]++;
-     }else if(IR_read[i] > IR_EDGE){
-        IR_hitCount[i] = 0;
-     }
-      
-     if(IR_hitCount[i]>IR_HIT_BUFFER){
-        IR_hitCount[i] = IR_HIT_BUFFER+1;  //Prevent overflow by capping element size
-        IR_flag = 1;
-        return i;
-     }
-  }*/
+  
   IR_flag = 0;
   return -1;
 }
@@ -715,10 +610,7 @@ void RightPICSendSerial(int angle, int spd){
       Serial2.print(spd); 
       delay(15);
       Serial2.print('S');
-     /* Serial.print("R: ang ");
-      Serial.print(angle);
-      Serial.print('\n');
-      */
+   
       return;
 }
 //Takes a String representing an integer and converts it to an int
@@ -760,19 +652,7 @@ void serialEvent(){
     }else if(inChar <= 57 && inChar >= 48){
       inputString += inChar;
     }
-  }/*
-  while(Serial1.available()){
-    char a1 = (char) Serial1.read();
-    Serial.print("Left PIC angle: ");
-    Serial.print(a1);
-    Serial.print("\n");
   }
-  while(Serial2.available()){
-    char a2 = (char) Serial2.read();
-    Serial.print("Right PIC angle: ");
-    Serial.print(a2);
-    Serial.print("\n");
-  }*/
 }
 
 void handleAntennaReadings(){
@@ -837,37 +717,21 @@ void handleAntennaReadings(){
      break;
  }
  
-//  Serial.print("In1, In2, Cal\t");
-//  Serial.print(voltage_1);
-//  Serial.print("\t");
-//  Serial.print(voltage_2);
-//  Serial.print("\t");
-//  Serial.print(caliset);
-//  Serial.print(" Position ");
-//  Serial.println(current_pos_micro);
-  
-//  Serial.print(" Agreed ");
-//  Serial.print(sample_array_f[case_num]);
-//  Serial.print(" Cast Ang ");
-//  Serial.print(angle_2_casters);
-  
-//  RightPICSendSerial(angle_2_casters, STOP_SPD);
-//  LeftPICSendSerial(angle_2_casters, STOP_SPD); 
   
 }
 
 int determine_direction_from_sample(uint16_t voltageReading_rise, uint16_t voltageReading_fall) {
   int diff = voltageReading_rise-voltageReading_fall;
-    if (voltageReading_rise < STOP_LEVEL_MAX ){
+    if (voltageReading_rise < STOP_LEVEL_MAX ){ //stop movement
       return STOP_MOV;
     } else {
-      if (diff < buffering &&  diff > -1*buffering) //(voltageReading_rise > (caliset - buffering) && voltageReading_rise < (caliset + buffering))  //drive forward
+      if (diff < buffering &&  diff > -1*buffering) //drive forward
       return HOLD_ANG;
     
-    else if (diff > 0)//(voltageReading_rise < (caliset -buffering)) //turn  voltage > (caliset + buffering)
+    else if (diff > 0)//turn  voltage > (caliset + buffering)
     return DEC_ANG;
 
-    else if (diff < 0 )//(voltageReading_rise > (caliset + buffering))  //turn the other way
+    else if (diff < 0 )//turn the other way
     return INC_ANG;
   }
   return ERROR_CALC;
@@ -875,19 +739,21 @@ int determine_direction_from_sample(uint16_t voltageReading_rise, uint16_t volta
 
 
 
-ISR(TIMER2_COMPA_vect){//timer2 interrupt 1kHz
+ISR(TIMER2_COMPA_vect){//timer2 interrupt @ 1kHz
+  //Toggle switching waveform
   digitalWrite(ANT_WAVEFORM,toggle);
   toggle = toggle^1;
-  edge_count+=1;
-  if (second_sample_flag && samples_full != 1) {
+  
+  edge_count+=1; // add count to num of edges caused
+  
+  if (second_sample_flag && samples_full != 1) {//if a sample on rising edge was taken, but the buffer is not full, take a sample for falling edge
    voltage_2 =  analogRead(SPEAKER_FROM_WALKIETALKIE);
    voltageReadings_2[num_samples_taken] = voltage_2;
    num_samples_taken +=1;
    second_sample_flag = 0;
   }
-  if (edge_count >= sample_delay*2){
-    
-    estate = !estate;
+  
+  if (edge_count >= sample_delay*2){ //if the num of edges counted is above the delay, take a rising edge reading
     edge_count = 0;
     voltage_1 = analogRead(SPEAKER_FROM_WALKIETALKIE);
     if (num_samples_taken < antenna_sample_size){
@@ -898,9 +764,11 @@ ISR(TIMER2_COMPA_vect){//timer2 interrupt 1kHz
     }
     ant_read_flag = 1;
   }
-  //SERVO Handling
-  if (interrupt_count_servo >= DELAY){
-    if (move_enabled){
+  
+  
+  //SERVO Handling. Handle servo movements
+  if (interrupt_count_servo >= DELAY){ // if count from last servo handling is above delay, move servo
+    if (move_enabled){ // increase current position if it is not at goal angle
       if (current_pos_micro > goal_ang_micro ){
         current_pos_micro-=STEP;
         finished_move = 0;
@@ -911,42 +779,41 @@ ISR(TIMER2_COMPA_vect){//timer2 interrupt 1kHz
         finished_move = 1;
       }
     }
-    interrupt_count_servo = 0;
-    if (current_pos_micro > MAX_POS) current_pos_micro = MAX_POS;
+    interrupt_count_servo = 0; //clear flag
+    if (current_pos_micro > MAX_POS) current_pos_micro = MAX_POS; //constrain servo
     if (current_pos_micro < MIN_POS) current_pos_micro = MIN_POS;
-    antenna_servo.writeMicroseconds(current_pos_micro);
+    antenna_servo.writeMicroseconds(current_pos_micro); //set servo
     //
   } else interrupt_count_servo+=1;
-  //moveantenna
-  //antenna_servo.writeMicroseconds(current_pos_micro);
-  //Antenna Reading
-  //Sensor Timer
-  if (sensor_timer_count >= 100) {
+  
+  //flag for sensor readings
+  if (sensor_timer_count >= SENSOR_READING_DELAY) {
     sensor_timer_count = 0;
     timer_flag = 1;
   } else sensor_timer_count +=1;
 }
 
+
 void powerUpRadio() {
-  boolean turnOn = true; //default to turn the radio on
+  boolean turnOff = true; //default to turn the radio off
   digitalWrite(RADIO_VOL_UP,HIGH);
-  delay(150);
+  delay(150); //delay till sound comes on (if on)
   int testOn = analogRead(SPEAKER_FROM_WALKIETALKIE);
   if (testOn > STOP_LEVEL_MAX){ //the radio is already on
-    turnOn = false;
+    turnOff = false;
   } 
-  delay(350);
-  digitalWrite(RADIO_VOL_UP,LOW);
+  delay(350); // finish hold
+  digitalWrite(RADIO_VOL_UP,LOW); // release volume button
   delay(500);
   
-  if (turnOn){
+  if (turnOff){ //if radio is not on
   
-  digitalWrite(RADIO_POWER,HIGH);
+  digitalWrite(RADIO_POWER,HIGH); //power up
   delay(1500);
   digitalWrite(RADIO_POWER,LOW);
   delay(1000);
   
-  digitalWrite(RADIO_VOL_UP,HIGH);
+  digitalWrite(RADIO_VOL_UP,HIGH);//next three blocks increase volume
   delay(500);
   digitalWrite(RADIO_VOL_UP,LOW);
   delay(500);
